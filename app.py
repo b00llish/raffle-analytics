9a6c058d0773007ffe2613a7a1d794233e660d16
b00llish <b00llish@pm.me>
2023-02-25 19:10:37 -0500

    solving for heroku crashes - adjustments to app.py, .gitignore and requirements.txt

--- app.py
+++ app.py
@@ -4,12 +4,16 @@ import numpy as np
 import pandas as pd
 import matplotlib as mpl
 import gunicorn
+from whitenoise import WhiteNoise
 
 # Instantiate dash app
 app = Dash(__name__, external_stylesheets=[dbc.themes.FLATLY])
 
 # Reference the underlying flask app (Used by gunicorn webserver in Heroku production deployment)
 server = app.server
+
+# Enable Whitenoise for serving static files from Heroku (the /static folder is seen as root by Heroku)
+server.wsgi_app = WhiteNoise(server.wsgi_app, root='static/')
 
 def create_dash_layout(app):
     # Set browser tab title
