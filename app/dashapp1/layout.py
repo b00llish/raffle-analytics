60a9966ffcc513635f9076b598ea8476458d91de
b00llish <b00llish@pm.me>
2023-02-26 00:06:02 -0500

    developing dash within flask file structure

---
+++ app/dashapp1/layout.py
@@ -0,0 +1,8 @@
+# import dash_core_components as dcc
+from dash import html as html
+
+layout = html.Div(id='main', children=[
+    # html.H1(id='username'),
+    html.H1('Stock Tickers')
+
+], style={'width': '500'})
---
+++ flask_app.py
@@ -0,0 +1,8 @@
+from dash import Dash, callback, html, dcc
+import dash_bootstrap_components as dbc
+import os
+import numpy as np
+import pandas as pd
+import matplotlib as mpl
+import gunicorn
+from whitenoise import WhiteNoise
