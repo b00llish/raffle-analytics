b72362b8e9853c7aaa3c1250e86df36e734111ac
b00llish <b00llish@pm.me>
2023-02-25 21:19:38 -0500

    exploring pipeline

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from dash import Dash, callback, html, dcc
 import dash_bootstrap_components as dbc
+import os
 import numpy as np
 import pandas as pd
 import matplotlib as mpl
@@ -8,6 +9,10 @@ from whitenoise import WhiteNoise
 
 # Instantiate dash app
 app = Dash(__name__, external_stylesheets=[dbc.themes.FLATLY])
+env_config = os.getenv("APP_SETTINGS", "config.DevelopmentConfig")
+app.config.from_object(env_config)
+
+secret_key = app.config.get("SECRET_KEY")
 
 # Reference the underlying flask app (Used by gunicorn webserver in Heroku production deployment)
 server = app.server
@@ -15,9 +20,10 @@ server = app.server
 # Enable Whitenoise for serving static files from Heroku (the /static folder is seen as root by Heroku)
 server.wsgi_app = WhiteNoise(server.wsgi_app, root='static/')
 
+
 def create_dash_layout(app):
     # Set browser tab title
-    app.title = "Rafffle Analytics"
+    app.title = f'Rafffle Analytics - {secret_key}'
 
     # Header
     header = html.Div([html.Br(), dcc.Markdown(
---
+++ config.py
@@ -0,0 +1,20 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config:
+    DEBUG = False
+    DEVELOPMENT = False
+    SECRET_KEY = os.getenv("SECRET_KEY", "this-is-the-default-key")
+
+
+class ProductionConfig(Config):
+    pass
+
+
+class StagingConfig(Config):
+    DEBUG = True
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    DEVELOPMENT = True
