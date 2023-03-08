d493ed23f76be8d8976d2071ed87db65766e23b4
b00llish <b00llish@pm.me>
2023-03-08 02:49:30 -0500

    reducing time span for flipside queries

--- data/queries/ends_flipside.sql
+++ data/queries/ends_flipside.sql
@@ -6,7 +6,7 @@ FROM
   solana.core.fact_transactions t
 WHERE
   -- date range for data:
-  date_trunc('day', dt_end) > CURRENT_DATE - interval '2 days'
+  date_trunc('day', dt_end) > CURRENT_DATE - interval '1 days'
   -- date_trunc('day', date_end) >= '2022-10-31' -- inclusive
   -- AND date_trunc('day', date_end) < '2023-01-01' --exclusive
 
